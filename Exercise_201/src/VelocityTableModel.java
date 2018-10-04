d5ea6bd962d47a34ef5fbf56a39bc7464be0badc
johannes <suljoc16@new.edvhtl.at>
2018-10-04 08:24:59 +0200

    changes

--- Exercise_201/src/VelocityTableModel.java
+++ Exercise_201/src/VelocityTableModel.java
@@ -1,13 +1,32 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author JoZuG
- */
-public class VelocityTableModel {
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+
+
+public class VelocityTableModel extends AbstractTableModel {
+    
+    private static String[] colNames = {"Datum", "Uhrzeit", "Kennzeichen", "Gemessen", "Erlaubt", "Übertretung"};
+    private ArrayList<Measurement> measure = new ArrayList<>();
+    
+    public void add(Measurement m) {
+        measure.add(m);
+        fireTableRowsInserted(measure.size() - 1, measure.size() - 1);
+   }
+    
+    @Override
+    public int getRowCount() {
+        return measure.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return 5;
+    }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Measurement m = measure.get(rowIndex);
+        return m;
+    }
     
 }
